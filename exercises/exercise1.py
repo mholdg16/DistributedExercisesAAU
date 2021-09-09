876dfbb900cb533a086d25f7dc485fa15b1a88cb
mholdg16 <mholdg16@student.aau.dk>
2021-09-09 22:26:19 +0200

    Random, circular at 5 msgs (livelock still occurs)

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 from emulators.Device import Device
 from emulators.Medium import Medium
 from emulators.MessageStub import MessageStub
@@ -23,10 +26,69 @@ class Gossip(Device):
         self._secrets = set([index])
 
     def run(self):
-        # the following is your termination condition, but where should it be placed?
-        # if len(self._secrets) == self.number_of_devices():
-        #    return
-        return
+        while True:
+            # choose a random receiver (that is not self)
+            p = self.index()
+            while p == self.index():
+                p = random.randint(0, self.number_of_devices() - 1)
+            
+            while True:
+                ingoing = self.medium().receive()
+
+                if ingoing is None:
+                    break
+
+                # join the received secrets with known secrets
+                self._secrets.update(ingoing.secrets)
+            
+            self.medium().send(GossipMessage(self.index(), p, self._secrets))
+
+            # the following is your termination condition, but where should it be placed?
+            if len(self._secrets) == self.number_of_devices():
+                break
+
+            # wait ~[0,1] seconds to avoid livelock
+            time.sleep(random.random())
+
+
+    def print_result(self):
+        print(f'\tDevice {self.index()} got secrets: {self._secrets}')
+
+
+class GossipCircular(Device):
+
+    def __init__(self, index: int, number_of_devices: int, medium: Medium):
+        super().__init__(index, number_of_devices, medium)
+        # for this exercise we use the index as the "secret", but it could have been a new routing-table (for instance)
+        # or sharing of all the public keys in a cryptographic system
+        self._secrets = set([index])
+
+    def run(self):
+        while True:
+            # choose receiver (left or right)
+            if random.randint(0,1) == 1:
+                p = (self.index() + 1) % self.number_of_devices()
+            else:
+                p = (self.index() - 1) % self.number_of_devices()
+            
+            while True:
+                ingoing = self.medium().receive()
+
+                if ingoing is None:
+                    break
+
+                # join the received secrets with known secrets
+                self._secrets.update(ingoing.secrets)
+            
+            self.medium().send(GossipMessage(self.index(), p, self._secrets))
+
+            # the following is your termination condition, but where should it be placed?
+            if len(self._secrets) == self.number_of_devices():
+                break
+
+            # wait ~[0,1] seconds to avoid livelock
+            time.sleep(random.random())
+
 
     def print_result(self):
         print(f'\tDevice {self.index()} got secrets: {self._secrets}')
